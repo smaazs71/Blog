a34d107a8a99b245d789e0405ebe7504a9a7b4c1
Maaz <smaazs71@gmail.com>
2024-03-14 07:46:15 +0530

    resolved build errors =>used global instead of globalThis.

--- utils/connect.ts
+++ utils/connect.ts
@@ -8,11 +8,11 @@ declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+const prisma = global.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 // let prisma;
 // if (process.env.NODE_ENV === "production") {
